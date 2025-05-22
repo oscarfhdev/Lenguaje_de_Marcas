d2b3fda99dfeaedafc98a27cc2ecb12ed2763828
oscarfhdev <oscarfh.dev@gmail.com>
2025-05-22 22:57:45 +0200

    CREATE src/app/homepage --> new component

---
+++ Angular/CV_Angular/src/app/homepage/homepage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [],
+  standalone: true,
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
